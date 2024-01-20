5f19d3f80275b77c8117db9f7ea1a79b48de543f
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-19 23:26:25 -0300

    Implementando lógica da injeção de dependência.

--- main/src/DependencyInjector/DependencyInjector.java
+++ main/src/DependencyInjector/DependencyInjector.java
@@ -10,4 +10,35 @@ public class DependencyInjector {
     public <T> void register(Class<T> interfaceType, Class<? extends T> implementationType) {
         mappings.put(interfaceType, implementationType);
     }
+
+    public <T> T resolve(Class<T> interfaceType) throws Exception {
+        Class<?> implementationType = mappings.get(interfaceType);
+
+        if(implementationType == null) {
+            throw new RuntimeException("Implementation not registered for" + interfaceType);
+        }
+
+        Constructor<?>[] constructors = implementationType.getDeclaredConstructors();
+        Constructor<?> constructor = constructors[0];
+
+        Object implementationInstance;
+        if(constructor.getParameterCount() > 0) {
+            Object[] params = new Object[constructor.getParameterCount()];
+            for(int i = 0; i < params.length; i++) {
+                params[i] = resolve(constructor.getParameterTypes()[i]);
+            }
+            implementationInstance = constructor.newInstance(params);
+        } else {
+            implementationInstance = constructor.newInstance();
+        }
+
+        for(var field : implementationType.getDeclaredFields()) {
+            if(field.getType().isInterface()) {
+                field.setAccessible(true);
+                field.set(implementationInstance, resolve((Class<?>) field.getType()));
+            }
+        }
+
+        return interfaceType.cast(implementationInstance);
+    }
 }
--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,20 @@
+import DependencyInjector.DependencyInjector;
+import Logger.ConsoleLogger;
+import Logger.ILogger;
+import Service.Service;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            DependencyInjector di = new DependencyInjector();
+
+            di.register(ILogger.class, ConsoleLogger.class);
+            di.register(Service.class, Service.class);
+
+            Service service = di.resolve(Service.class);
+            service.doSomething();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
