0ce54ce16ef8eba850795b5cc721512867c2a4cf
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-19 23:25:31 -0300

    Criando classe de exemplo que será dependência injetada

---
+++ main/src/Service/Service.java
@@ -0,0 +1,15 @@
+package Service;
+
+import Logger.ILogger;
+
+public class Service {
+    private final ILogger logger;
+
+    public Service(ILogger logger) {
+        this.logger = logger;
+    }
+
+    public void doSomething() {
+        System.out.println("just a test...");
+    }
+}
