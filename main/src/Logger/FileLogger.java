37e5c83f89ed1b1efb6ced4c83a67cd540eddb60
Diogo Martins de Assis <diogomrtsa@gmail.com>
2024-01-21 12:45:25 -0300

    Update FileLogger.java

--- main/src/Logger/FileLogger.java
+++ main/src/Logger/FileLogger.java
@@ -1,12 +1,6 @@
 package Logger;
 
 public class FileLogger implements ILogger {
-    private final String filePath;
-
-    public FileLogger(String filePath) {
-        this.filePath = filePath;
-    }
-
     @Override
     public void log(String message) {
         System.out.println("File: " + message);
